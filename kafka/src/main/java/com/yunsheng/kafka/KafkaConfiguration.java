4a854165c55ec42c0f3c73db05a8f89bda5e2cc9
yunsheng <helloyunsheng@126.com>
2019-01-27 23:33:46 +0800

    kafka

---
+++ kafka/src/main/java/com/yunsheng/kafka/KafkaConfiguration.java
@@ -0,0 +1,10 @@
+package com.yunsheng.kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfiguration {
+
+}
---
+++ kafka/src/main/java/com/yunsheng/kafka/MsgConsumer.java
@@ -0,0 +1,15 @@
+package com.yunsheng.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MsgConsumer {
+
+    @KafkaListener(topics = {"topic-1","topic-2"})
+    public void processMessage(String content) {
+
+        System.out.println("消息被消费"+content);
+    }
+    
+}
