d21461ce5854ca1b1c37bc967370408f50656e50
杨云胜 <yangyunsheng@haier.com>
2019-01-28 15:32:29 +0800

    kafka使用

--- kafka/src/main/java/com/yunsheng/kafka/KafkaApplication.java
+++ kafka/src/main/java/com/yunsheng/kafka/KafkaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = {"com.yunsheng.kafka"})
+//@ComponentScan(value = {"com.yunsheng.kafka"})
 public class KafkaApplication {
 
     public static void main(String[] args) {
--- kafka/src/test/java/com/yunsheng/kafka/KafkaApplicationTests.java
+++ kafka/src/test/java/com/yunsheng/kafka/KafkaApplicationTests.java
@@ -1,24 +1,12 @@
 package com.yunsheng.kafka;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = KafkaApplication.class)
 public class KafkaApplicationTests {
 
-    @Autowired
-    private MsgProducer msgProducer;
 
-    @Test
-    public void test() throws Exception {
-
-        msgProducer.sendMessage("topic-1", "topic--------1");
-        msgProducer.sendMessage("topic-2", "topic--------2");
-    }
 
 }
 
