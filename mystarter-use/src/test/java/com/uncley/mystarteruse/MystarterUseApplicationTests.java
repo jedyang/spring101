d8a8f535ead2322ae6383d57243076fa650a6231
杨云胜 <yangyunsheng@haier.com>
2019-08-06 12:03:43 +0800

    使用nacos做配置中心

---
+++ mystarter-use/src/test/java/com/uncley/mystarteruse/MystarterUseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uncley.mystarteruse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MystarterUseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
